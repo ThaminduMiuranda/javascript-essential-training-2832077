40ded7229178fed2e01db52cf7addc0313415787
ThaminduMiuranda <thaminduthilina@gmail.com>
2024-09-10 18:24:38 +0530

    Refactor script.js to remove unnecessary code and console logs

--- test/0_01/script.js
+++
@@ -1,8 +0,0 @@
-let course = new Map();
-
-course.set("react", { description: "ui" });
-course.set("jest", { description: "testing" });
-
-console.log(course);
-console.log(course.react);
-console.log(course.get("react"));
